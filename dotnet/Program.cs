006b19a7ed261e5f522427c6844854ca724a6d64
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-02-12 11:34:19 +0530

    wip

---
+++ dotnet/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HttpClientLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunLoop(int.Parse(args.FirstOrDefault() ?? "8123"))
+                .Wait();
+        }
+
+        private static async Task RunLoop(int port)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var handler = new SocketsHttpHandler()
+            {
+                
+            };
+
+
+            var client = new HttpClient(handler);
+
+            var i = 0;
+
+            while(true)
+            {
+
+                try
+                {
+                    var url = $"https://localhost:{port}/{i++}";
+                    var r = await client.GetStringAsync(url);
+                    Console.WriteLine($"{url} -> {r}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                await Task.Delay(10000);
+            }
+
+        }
+    }
+}
---
+++ http-server/index.js
@@ -0,0 +1,91 @@
+import * as forge from "node-forge";
+import tls from "node:tls";
+import http2 from "node:http2";
+
+const port = 8123;
+
+const SNICallback = (name, cb) => {
+    try {
+        console.log(`SNICallback: ${name}`);
+        const { key, cert } = createSelfSignedCert();
+        cb(null, tls.createSecureContext({ key, cert }));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const httpServer = http2.createSecureServer({
+    SNICallback,
+    allowHTTP1: true
+}, (req, res) => {
+    try {
+        console.log(`Authority: ${req.authority ?? req.headers.host}`);
+        res.end("ok");
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+httpServer.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
+
+
+function createSelfSignedCert() {
+
+    let key;
+    let cert;
+
+
+    const pki = forge.default.pki;
+
+    // generate a key pair or use one you have already
+    const keys = pki.rsa.generateKeyPair(2048);
+
+    // create a new certificate
+    const crt = pki.createCertificate();
+
+    // fill the required fields
+    crt.publicKey = keys.publicKey;
+    crt.serialNumber = '01';
+    crt.validity.notBefore = new Date();
+    crt.validity.notAfter = new Date();
+    crt.validity.notAfter.setFullYear(crt.validity.notBefore.getFullYear() + 40);
+
+    // use your own attributes here, or supply a csr (check the docs)
+    const attrs = [
+        {
+            name: 'commonName',
+            value: 'dev.socialmail.in'
+        }, {
+            name: 'countryName',
+            value: 'IN'
+        }, {
+            shortName: 'ST',
+            value: 'Maharashtra'
+        }, {
+            name: 'localityName',
+            value: 'Navi Mumbai'
+        }, {
+            name: 'organizationName',
+            value: 'NeuroSpeech Technologies Pvt Ltd'
+        }, {
+            shortName: 'OU',
+            value: 'Test'
+        }
+    ];
+
+    // here we set subject and issuer as the same one
+    crt.setSubject(attrs);
+    crt.setIssuer(attrs);
+
+    // the actual certificate signing
+    crt.sign(keys.privateKey);
+
+    // now convert the Forge certificate to PEM format
+    cert = pki.certificateToPem(crt);
+    key = pki.privateKeyToPem(keys.privateKey);
+
+    return { key, cert };
+
+}
